1bea542ed3d568a3834c5b1e58e1adf783d7a033
SHUBHAM JAIN <Shubhamjain6197@users.noreply.github.com>
2021-04-20 12:55:16 +0530

    Update zabbix-agent-ubuntu.sh

--- scripts/zabbix-agent-ubuntu.sh
+++ scripts/zabbix-agent-ubuntu.sh
@@ -2,9 +2,6 @@
 #!/bin/bash
 serverip=$1 # or server ip you can provide
 metadatastring=$2
-#hostn=$(curl http://169.254.169.254/latest/meta-data/hostname/) #optinal line
-#hostnamectl set-hostname $hostn-discovery
-#hostnamectl set-hostname $hostn-$metadatastring #optinal line
 apt update -y
 apt install zabbix-agent
 sed -i -- 's/Server=127.0.0.1/Server='$serverip'/g' /etc/zabbix/zabbix_agentd.conf
@@ -14,5 +11,3 @@ echo "HostMetadata=$metadatastring" >> /etc/zabbix/zabbix_agentd.conf
 systemctl restart zabbix-agent
 systemctl enable zabbix-agent
 systemctl status zabbix-agent
-#reboot #optinal line
-
