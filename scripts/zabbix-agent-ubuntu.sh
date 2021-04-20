d5b8d5cc345dcd1b92cb379b8202df60362d1324
SHUBHAM JAIN <Shubhamjain6197@users.noreply.github.com>
2021-04-20 12:54:48 +0530

    Update zabbix-agent-ubuntu.sh

--- scripts/zabbix-agent-ubuntu.sh
+++ scripts/zabbix-agent-ubuntu.sh
@@ -1,14 +1,14 @@
-#Note - This script takes internalelb & metadatastring as input at runtime sh script.sh internal-dns-name free(env)-string
+#Note - This script takes serverip & metadatastring as input at runtime sh script.sh serverip free(env)-string
 #!/bin/bash
-internalelb=$1 # or server ip you can provide
+serverip=$1 # or server ip you can provide
 metadatastring=$2
 #hostn=$(curl http://169.254.169.254/latest/meta-data/hostname/) #optinal line
 #hostnamectl set-hostname $hostn-discovery
 #hostnamectl set-hostname $hostn-$metadatastring #optinal line
 apt update -y
 apt install zabbix-agent
-sed -i -- 's/Server=127.0.0.1/Server='$internalelb'/g' /etc/zabbix/zabbix_agentd.conf
-sed -i -- 's/ServerActive=127.0.0.1/ServerActive='$internalelb'/g' /etc/zabbix/zabbix_agentd.conf
+sed -i -- 's/Server=127.0.0.1/Server='$serverip'/g' /etc/zabbix/zabbix_agentd.conf
+sed -i -- 's/ServerActive=127.0.0.1/ServerActive='$serverip'/g' /etc/zabbix/zabbix_agentd.conf
 sed -i -- 's/Hostname=Zabbix server/#Hostname=Zabbix server/g' /etc/zabbix/zabbix_agentd.conf
 echo "HostMetadata=$metadatastring" >> /etc/zabbix/zabbix_agentd.conf
 systemctl restart zabbix-agent
