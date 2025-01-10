4979ac66e73067ac5ef9ab611171b7b8b10f7d45
thatkide <3608003+thatkide@users.noreply.github.com>
2025-01-10 12:06:30 -0500

    updated to zabbix agent 2 added additional variables for psk settings

--- scripts/zabbix-agent-rhel7.sh
+++ scripts/zabbix-agent-rhel7.sh
@@ -2,13 +2,23 @@
 #!/bin/bash
 serverip=$1
 metadatastring=$2
-rpm -Uvh https://repo.zabbix.com/zabbix/5.2/rhel/7/x86_64/zabbix-release-5.2-1.el7.noarch.rpm
-yum clean all
-yum install zabbix-agent -y
-sed -i -- 's/Server=127.0.0.1/Server='$serverip'/g' /etc/zabbix/zabbix_agentd.conf
-sed -i -- 's/ServerActive=127.0.0.1/ServerActive='$serverip'/g' /etc/zabbix/zabbix_agentd.conf
-sed -i -- 's/Hostname=Zabbix server/#Hostname=Zabbix server/g' /etc/zabbix/zabbix_agentd.conf
-echo "HostMetadata=$metadatastring" >> /etc/zabbix/zabbix_agentd.conf
-systemctl restart zabbix-agent
-systemctl enable zabbix-agent
-systemctl status zabbix-agent
+tlspskid=$3
+tslpskval=$4
+rpm -Uvh rpm -Uvh https://repo.zabbix.com/zabbix/7.2/release/alma/9/noarch/zabbix-release-latest-7.2.el9.noarch.rpm
+dnf clean all
+dnf install zabbix-agent2 zabbix-agent2-plugin-* -y
+sed -i -- 's/Server=127.0.0.1/Server='$serverip'/g' /etc/zabbix/zabbix_agent2.conf
+sed -i -- 's/ServerActive=127.0.0.1/ServerActive='$serverip'/g' /etc/zabbix/zabbix_agent2.conf
+sed -i -- 's/Hostname=Zabbix server/#Hostname=Zabbix server/g' /etc/zabbix/zabbix_agent2.conf
+sed -i '/# HostnameItem=system.hostname/a HostnameItem=system.hostname' /etc/zabbix/zabbix_agent2.conf
+sed -i '/# HostMetadata=/a HostMetadata=$metadatastring' /etc/zabbix/zabbix_agent2.conf
+sed -i '/# TLSAccept=unencrypted/a TLSAccept=psk'  /etc/zabbix/zabbix_agent2.conf
+sed -i '/# TLSConnect=unencrypted/a TLSConnect=psk'  /etc/zabbix/zabbix_agent2.conf
+sed -i '/# TLSPSKIdentity=/a TLSPSKIdentity=$pskidentity' /etc/zabbix/zabbix_agent2.conf
+sed -i '/# TLSPSKFile=/a TLSPSKFile=/etc/zabbix/zabbix_agent.psk' /etc/zabbix/zabbix_agent2.conf
+echo "$tlspskval" >> /etc/zabbix/zabbix_agent2.conf
+systemctl restart zabbix-agent2
+systemctl enable zabbix-agent2
+systemctl status zabbix-agent2
+
+
--- scripts/zabbix-agent-ubuntu.sh
+++ scripts/zabbix-agent-ubuntu.sh
@@ -2,12 +2,21 @@
 #!/bin/bash
 serverip=$1 # or server ip you can provide
 metadatastring=$2
+tlspskid=$3
+tslpskval=$4
+wget https://repo.zabbix.com/zabbix/7.2/release/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_7.2+ubuntu24.04_all.deb
+dpkg -i zabbix-release_latest_7.2+ubuntu24.04_all.deb
 apt update -y
-apt install zabbix-agent
-sed -i -- 's/Server=127.0.0.1/Server='$serverip'/g' /etc/zabbix/zabbix_agentd.conf
-sed -i -- 's/ServerActive=127.0.0.1/ServerActive='$serverip'/g' /etc/zabbix/zabbix_agentd.conf
-sed -i -- 's/Hostname=Zabbix server/#Hostname=Zabbix server/g' /etc/zabbix/zabbix_agentd.conf
-echo "HostMetadata=$metadatastring" >> /etc/zabbix/zabbix_agentd.conf
-systemctl restart zabbix-agent
-systemctl enable zabbix-agent
-systemctl status zabbix-agent
+apt install zabbix-agent2 zabbix-agent2-plugin-*
+sed -i -- 's/Server=127.0.0.1/Server='$serverip'/g' /etc/zabbix/zabbix_agent2.conf
+sed -i -- 's/ServerActive=127.0.0.1/ServerActive='$serverip'/g' /etc/zabbix/zabbix_agent2.conf
+sed -i -- 's/Hostname=Zabbix server/#Hostname=Zabbix server/g' /etc/zabbix/zabbix_agent2.conf
+sed -i '/'Hostname=Zabbix server'/Hostname=' 
+sed -i '/# TLSAccept=unencrypted/a TLSAccept=psk'  /etc/zabbix/zabbix_agent2.conf
+sed -i '/# TLSConnect=unencrypted/a TLSConnect=psk'  /etc/zabbix/zabbix_agent2.conf
+sed -i '/# TLSPSKIdentity=/a TLSPSKIdentity=$pskidentity' /etc/zabbix/zabbix_agent2.conf
+sed -i '/# TLSPSKFile=/a TLSPSKFile=$pskfile' /etc/zabbix/zabbix_agent2.conf
+echo "HostMetadata=$metadatastring" >> /etc/zabbix/zabbix_agent2.conf
+systemctl restart zabbix-agent2
+systemctl enable zabbix-agent2
+systemctl status zabbix-agent2
