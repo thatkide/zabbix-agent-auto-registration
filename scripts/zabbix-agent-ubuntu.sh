8644c4888e7680d8397396a6931a5e3c5e7fa6e2
thatkide <3608003+thatkide@users.noreply.github.com>
2025-01-14 12:49:28 -0500

    fixed typo, added -y to zabbix install

--- scripts/zabbix-agent-ubuntu.sh
+++ scripts/zabbix-agent-ubuntu.sh
@@ -3,11 +3,11 @@
 serverip=$1 # or server ip you can provide
 metadatastring=$2
 tlspskid=$3
-tslpskval=$4
+tlspskval=$4
 wget https://repo.zabbix.com/zabbix/7.2/release/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_7.2+ubuntu24.04_all.deb
 dpkg -i zabbix-release_latest_7.2+ubuntu24.04_all.deb
 apt update -y
-apt install zabbix-agent2 zabbix-agent2-plugin-*
+apt install zabbix-agent2 zabbix-agent2-plugin-* -y
 sed -i -- 's/Server=127.0.0.1/Server='$serverip'/g' /etc/zabbix/zabbix_agent2.conf
 sed -i -- 's/ServerActive=127.0.0.1/ServerActive='$serverip'/g' /etc/zabbix/zabbix_agent2.conf
 sed -i -- 's/Hostname=Zabbix server/#Hostname=Zabbix server/g' /etc/zabbix/zabbix_agent2.conf
@@ -18,6 +18,6 @@ sed -i '/# TLSConnect=unencrypted/a TLSConnect=psk'  /etc/zabbix/zabbix_agent2.c
 sed -i '/# TLSPSKIdentity=/a TLSPSKIdentity='$tlspskid /etc/zabbix/zabbix_agent2.conf
 sed -i '/# TLSPSKFile=/a TLSPSKFile=/etc/zabbix/zabbix_agent.psk' /etc/zabbix/zabbix_agent2.conf
 echo $tlspskval >> /etc/zabbix/zabbix_agent.psk
-systemctl restart zabbix-agent2
 systemctl enable zabbix-agent2
+systemctl restart zabbix-agent2
 systemctl status zabbix-agent2
